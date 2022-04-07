93e7cb2f87898bbfbd19069e6c950378089b7cec
sakshii2000 <88350866+sakshii2000@users.noreply.github.com>
2022-04-07 23:58:06 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+  './',
+  'index.html',
+  'script.js',
+  'style.css',
+  'about.html',
+  'blogs.html',
+  'cart.html',
+  'contact.html',
+  'login.html',
+  'product.html',
+  'register.html',
+];
+
+self.addEventListener('install', async event => {
+  const cache = await caches.open(cacheName); 
+  await cache.addAll(staticAssets); 
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+  const cache = await caches.open(cacheName); 
+  const cachedResponse = await cache.match(req); 
+  return cachedResponse || fetch(req); 
+}
